93c0c8c50c373189d721c9527f0ae13a933b7b32
mickeykagamine <905110311@qq.com>
2018-02-21 19:00:56 +0800

    first commit

---
+++ c0104.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    double x;
+    scanf("%lf",&x);
+    x = x * 9 / 5 + 32;
+    printf("%.1lf\n",x);
+    system("pause");
+    return 0;
+    
+}
+    
+    
+    
+    
+    
+
+
